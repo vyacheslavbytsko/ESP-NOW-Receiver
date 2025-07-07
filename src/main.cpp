531052ad523b6606a6df4073a641e061a29703f1
Vyacheslav Bytsko <169024760+vyacheslavbytsko@users.noreply.github.com>
2025-07-07 17:40:57 +0300

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,3 @@
-/*#include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}*/
-
 #include <WiFi.h>
 #include <esp_now.h>
 
